5257158aff8c1e639c4bf97d17715a35f125f21d
Yuvaresh Srinivasan <yuvaresh581@gmail.com>
2026-03-31 13:21:20 +0530

    Added actual JSON file to store user data (Will be enhanced with MERN techniques in future)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 
@@ -12,37 +13,38 @@ app.get("/", (req, res) => {
   res.send("Node backend working 🚀");
 });
 
-let users = []; // temporary storage
-
 app.post("/signup", (req, res) => {
   const { email, password } = req.body;
 
+  // reload latest users
+  users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
+
+  // check if exists
+  if (users.find(u => u.email === email)) {
+    return res.json({ message: "User already exists" });
+  }
+
   users.push({ email, password });
 
-  console.log("Users:", users);
+  fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
-  res.json({
-    message: "Signup successful"
-  });
+  res.json({ message: "Signup successful" });
 });
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
+
+  users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
 
   const user = users.find(
     u => u.email === email && u.password === password
   );
 
   if (!user) {
-    return res.status(401).json({
-      message: "Invalid credentials"
-    });
+    return res.status(401).json({ message: "Invalid credentials" });
   }
 
-  res.json({
-    message: "Login successful",
-    user
-  });
+  res.json({ message: "Login successful", user });
 });
 
 // Start server
